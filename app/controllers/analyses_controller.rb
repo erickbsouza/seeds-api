ce9c4f349aca9e352e7423af0eef73268e260e3f
Erick Barroso de Souza <erick.souza@pge.ce.gov.br>
2023-09-17 20:20:02 -0300

    analises

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -6,8 +6,6 @@ class AnalysesController < ApplicationController
 
         
         if @analyzes.size > 0
-            #render json: {data: @analyzes, message: 'Análises encontradas'}
-            #render json: @analyzes.to_json(include: :benefited)
             render json: {data: @analyzes, message: 'Análises encontradas'}.to_json(include: :benefited)
         else
             render json: {data:[], message: 'Nenhuma análise encontrada'}, status: :no_content
@@ -18,7 +16,6 @@ class AnalysesController < ApplicationController
         @analyse = Analyse.includes([:analysis_results,:benefited]).find(params[:id])
 
         render json: @analyse.to_json(root: "data", include: [:benefited, :analysis_results])
-        #render json: {data: analyse_full}
     end
 
 end
